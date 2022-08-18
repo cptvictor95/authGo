acc43feaf1a8e4fa8d5d2b6d1aa8015ed12e77b1
cptvictor95 <cpt.victor@hotmail.com>
2022-08-18 19:23:41 -0300

    upgrade server main route to have logs

--- cmd/authGo/main.go
+++ cmd/authGo/main.go
@@ -2,39 +2,48 @@ package main
 
 import (
 	"fmt"
-	"html"
-	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func main() {
-	// First hello world route
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		myName := "world!"
-		fmt.Fprintf(w, "Hello, %s", html.EscapeString(myName))
+	// Start the server, listen and log
+	addr := "localhost:4200"
+	log.Printf("Server is up and running on http://%s/", addr)
+	log.Fatal((http.ListenAndServe(addr, nil)))
+
+	// Main API route
+	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Auth server is ON!")
 	})
 
+	// First hello world route
+	// http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+	// 	myName := "Victor!"
+	// 	fmt.Fprintf(w, "Hello, %s", html.EscapeString(myName))
+	// })
+
 	// Second hello world on another thread
-	go func ()  {
-		resp, err := http.Get("http://localhost:4200/hello")
+	// go func ()  {
+	// 	resp, err := http.Get("http://localhost:4200/hello")
 
-		body, err := ioutil.ReadAll(resp.Body)
+	// 	body, err := ioutil.ReadAll(resp.Body)
 
-		if err != nil {
-			fmt.Println(err)
+	// 	if err != nil {
+	// 		fmt.Println(err)
 
-			return
-		}
+	// 		return
+	// 	}
 
-		fmt.Println(string(body))
-
-		// body := make([]byte, 420)
+	// 	fmt.Println(string(body))
 		
-		// bodySize, err := resp.Body.Read(body)
-		// fmt.Println(resp.Status)
-		// fmt.Println(string(body[:bodySize])) 
-		// fmt.Println(err)
-	}()
+	// 	body := make([]byte, 420)
+		
+	// 	bodySize, err := resp.Body.Read(body)
+	// 	fmt.Println(resp.Status)
+	// 	fmt.Println(string(body[:bodySize]))
+	// 	fmt.Println(err)
+	// }()
 
-	http.ListenAndServe("localhost:4200", nil)
+	
 }
