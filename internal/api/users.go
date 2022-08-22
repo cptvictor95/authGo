7b14f9e89acb6f8463c2b1ec5fc68d1a13db594a
cptvictor95 <cpt.victor@hotmail.com>
2022-08-21 21:57:25 -0300

    create users map to simulate dummy data from database

--- internal/api/users.go
+++ internal/api/users.go
@@ -6,12 +6,28 @@ import (
 	"strings"
 )
 
+type User struct {
+	name string
+	age int
+}
+
 func UsersRoute() {
 	http.HandleFunc("/api/users/", func(w http.ResponseWriter, r *http.Request) {
 		userId := strings.TrimPrefix(r.URL.Path, "/api/users/")
 
 		if len(userId) == 0 {
-			fmt.Fprintf(w, "Users route is working, %s", r.URL.Path)
+			var users map[string]User
+
+			users = make(map[string]User)
+			users["qwe123"] = User{
+				"Victor", 27,
+			}
+			users["asd456"] = User{
+				"Thales", 28,
+			}
+
+			fmt.Printf("Users map: %q", users)
+			fmt.Fprintf(w, "Users route is working on path: %s \n Users map: %q", r.URL.Path, users)
 
 			return
 		}
